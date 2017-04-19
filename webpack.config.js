72d3656a943380adb3262a404e0c71c3af92d49c
Vladimir <dosandkv@gmail.com>
2017-04-19 18:20:16 +0300

    added babel dependencies;

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,25 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [/(node_modules)/, /\.spec\.jsx$/],
+        include: [
+          path.resolve(__dirname, './js')
+        ],
+        use: [
+          {
+            loader: 'babel-loader',
+            query: {
+              cacheDirectory: true
+            }
+          }
+        ]
+      }
+    ]
+  },
   devServer: {
     contentBase: path.resolve(__dirname, './'),
     compress: true,
